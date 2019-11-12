ef386b8308d5995a2ac74d3287f313ab0ba91b29
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-11-12 23:52:33 +0900

    Create #10952.c

---
+++ #10952.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    
+    for (;;){
+        scanf("%d %d", &a, &b);
+        if (a+b != 0){
+            printf("%d\n", a+b);
+        }
+        else
+            return 0;
+        
+    }
+    
+    return 0;
+}
